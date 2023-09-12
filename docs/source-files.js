affe649e37763c6d98a03fc2c8ab054113f1fc17
francesco-ismb <francesco.lubrano@linksfoundation.com>
2023-09-12 15:23:40 +0000

    Updated documentation

--- docs/source-files.js
+++ docs/source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"warp_backend":["",[["db_lib",[],["mod.rs","models.rs","schema.rs"]],["http_server",[],["mod.rs"]],["reservation_manager",[],["mod.rs"]],["ssh_lib",[],["mod.rs"]],["yaml_handle_cmd",[],["mod.rs"]],["yaml_prs_config",[],["mod.rs"]],["yaml_prs_cwl",[],["mod.rs"]]],["main.rs"]]\
+"warp_backend":["",[["db_lib",[],["mod.rs","models.rs","schema.rs"]],["http_server",[],["mod.rs"]],["reservation_manager",[],["mod.rs"]],["slurm_lib",[],["mod.rs"]],["yaml_handle_cmd",[],["mod.rs"]],["yaml_prs_config",[],["mod.rs"]],["yaml_prs_cwl",[],["mod.rs"]]],["main.rs"]]\
 }');
 createSourceSidebar();
